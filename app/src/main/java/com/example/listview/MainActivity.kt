a0ad84c715d1e4490e2c804e68054a6b2420c150
Shivangi <shivangit1111@gmail.com>
2023-04-10 11:52:44 +0530

    feat: show item in listView

--- app/src/main/java/com/example/listview/MainActivity.kt
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val arrayAdapter: ArrayAdapter<*>
+
+        val toDo = arrayOf(
+            "Add ListView in Xml", "load XML resource from our activity onCreate()", "Steve Smith",
+            "Kane Williamson", "Ross Taylor"
+        )
+
+        // access the listView from xml file
+        val mListView = findViewById<ListView>(R.id.userlist)
+        arrayAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1, toDo
+        )
+        mListView.adapter = arrayAdapter
+
     }
 }
